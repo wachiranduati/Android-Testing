26b0053313eefa70a47952c5b6e3255d40ba6a0b
Wachira Nduati <wachiranduati@gmail.com>
2020-12-03 19:31:27 +0300

    add before and after test methods

--- app/src/test/java/com/nduati/androidtesting/utils/CalculatorTest.kt
+++ app/src/test/java/com/nduati/androidtesting/utils/CalculatorTest.kt
@@ -2,6 +2,7 @@ package com.nduati.androidtesting.utils
 
 import junit.framework.TestCase
 import org.junit.Before
+import org.junit.Test
 
 class CalculatorTest : TestCase() {
 
@@ -12,7 +13,14 @@ class CalculatorTest : TestCase() {
         calc = Calculator()
     }
 
+    @Test
     fun testAddItems() {
         assertEquals(4, calc.addItems(2,2))
     }
+
+//    @Before
+//    covers class methods that are run before the @Test method is run
+
+//    @After
+//    This is run after the @Test is run
 }
